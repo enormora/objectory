8566f70605b0e425fc77516126977f171ba3c7b8
Christian Rackerseder <git@echooff.de>
2025-09-30 11:13:03 +0200

    Create basic factory function

--- source/main.ts
+++ source/main.ts
@@ -1,3 +1,13 @@
-export function main(): string {
-    return '';
+type ObjectoryFactory<ObjectShape> = {
+    readonly build: () => ObjectShape;
+};
+
+type GeneratorFunction<ObjectShape> = () => ObjectShape;
+
+export function createFactory<ObjectShape extends Record<string, unknown>>(
+    generatorFunction: GeneratorFunction<ObjectShape>
+): ObjectoryFactory<ObjectShape> {
+    return {
+        build: generatorFunction
+    };
 }
